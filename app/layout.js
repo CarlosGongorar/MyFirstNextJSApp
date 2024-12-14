d8c40c1ecd6b20ff62d880cb12b9775b12caf13e
CarlosGongorar <carlosgongorar50@gmail.com>
2024-12-14 15:51:07 -0500

    Adding Fonts from google fonts

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import Navbar from "../components/Navbar"
+import { Roboto } from "next/font/google"
 
 /** Este exporta las propiedades importantes de la pagina como titulo, descripcion keywords
  * y otros, ademas de eso. Hay mas cosas que se pueden meter de datos meta, pero toca buscar en la
@@ -9,11 +10,20 @@ export const metadata = {
   description: "Esta es la pagina principal de mi tienda",
   keywords: "tienda, ecommerce, yuyi"
 }
+
+
+/** Aca primero toca ejecutar roboto que es un objeto que recibe diferentes parametros antes de poder usarlo en algun sitio */
+const roboto = Roboto({
+  weight: ["100", "300", "400", "500", "700", "900"],
+  styles: ["italic", "normal"],
+  subsets: ["latin"],
+})
+
 /** Lo que contiene esto siempre se va a mostrar */
 export default function RootLayout({ children }) { /** Aca se le pasa los parametros hijos a la funcion que son las paginas que va a mostrar ademas se pueden añadir mas cosas */
   return (
     <html lang="en">
-      <body>
+      <body className={roboto.className}>
         <Navbar />
         {children}
         </body>
